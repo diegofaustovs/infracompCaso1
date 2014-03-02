8cfe3ff19e4e74c8d2bb59ac372fb1d21a9768d0
Diego Vanegas <diegofaustovs@gmail.com>
2014-03-02 15:56:06 -0500

    Servidor - Cliente implementado (Cliente Impl)

--- src/cliente/Cliente.java
+++ src/cliente/Cliente.java
@@ -3,8 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package cliente;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 /**
  *
@@ -12,5 +17,54 @@ package cliente;
  */
 public class Cliente
 {
-    
+
+    public final static String IP = "127.0.0.1";
+    public final static int PUERTO = 45405;
+
+    public static void main(String[] args) throws IOException
+    {
+        boolean ejecutar = true;
+        Socket socketSalida = null;
+        PrintWriter escritor = null;
+        BufferedReader lector = null;
+
+        try
+        {
+            socketSalida = new Socket(IP, PUERTO);
+            escritor = new PrintWriter(socketSalida.getOutputStream(), true);
+            lector = new BufferedReader(new InputStreamReader(socketSalida.getInputStream()));
+        }
+        catch (Exception e)
+        {
+            System.err.println("Exception: " + e.getMessage());
+            System.exit(1);
+        }
+
+        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        String fromServer, fromUser;
+
+        while ((fromServer = lector.readLine()) != null && ejecutar)
+        {
+            System.out.println("Servidor: " + fromServer);
+            if (fromServer.equalsIgnoreCase("Terminar"))
+            {
+                ejecutar = false;
+            }
+
+            if (ejecutar)
+            {
+                fromUser = stdIn.readLine();
+                if (fromUser != null)
+                {
+                    System.out.println("Cliente: " + fromUser);
+                    escritor.println(fromUser);
+                }
+            }
+        }
+
+        escritor.close();
+        lector.close();
+        socketSalida.close();
+        stdIn.close();
+    }
 }
